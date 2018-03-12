24021ae44c48d50f95b8098f02555fe52733c512
yusheng <yusheng@lehoolive.com>
2018-03-12 18:03:53 +0800

    Add contents

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+func main()  {
+	//Ask
+	input := flag.String("input","AB5,BC4,CD8,DC8,DE6,AD5,CE2,EB3,AE7","Please Input the Data")
+	data := strings.Split(*input,",")
+	for _,v := range data {
+		fmt.Println(v)
+		// Data Format
+	}
+
+	//Compute
+
+	//Answer
+}
+
+func DataFormat()  {
+
+	//Data Format"
+}
+
+func Compute()  {
+	// Distance
+
+	// Step Limit
+
+	// Shortest Route
+
+	// Distance Limit
+}
+
+func Answer()  {
+	// Println
+}
