4d1248615cbbc86a4a1a5cc285e5803cb2e37a92
yusheng <yusheng@lehoolive.com>
2018-03-14 01:19:45 +0800

    Add ShortestPath to Answer Q8/9

--- graph/graph_test.go
+++ graph/graph_test.go
@@ -18,13 +18,28 @@ func TestGraph_NewGraphFromString(t *testing.T) {
 func TestGraph_CalcDistance(t *testing.T) {
 	data := []string{"AB5","BC4","CD8","DC8","DE6","AD5","CE2","EB3","AE7"}
 	if gra,err := NewGraphFromString(data);err == nil {
-		fmt.Println(gra.CalcRouteDistance("A-B-C"))
+		fmt.Println(gra.CalcRouteDistance([]string{"A","D","C"}))
 	}
 }
 
 func TestGraph_BFSTraverse(t *testing.T) {
 	data := []string{"AB5","BC4","CD8","DC8","DE6","AD5","CE2","EB3","AE7"}
 	if gra,err := NewGraphFromString(data);err == nil {
-		fmt.Println(gra.BFSTraverse("A","C",4,true))
+		fmt.Println(gra.BFSTraverse("C","C",3,false))
+	}
+}
+
+func TestGraph_ShortestPath(t *testing.T) {
+	data := []string{"AB5","BC4","CD8","DC8","DE6","AD5","CE2","EB3","AE7"}
+	if gra,err := NewGraphFromString(data);err == nil {
+		//fmt.Println(gra.DifShortestPath("C","B"))
+		fmt.Println(gra.SameShortestPath("B"))
+	}
+}
+
+func TestGraph_BFSDistLimit(t *testing.T) {
+	data := []string{"AB5","BC4","CD8","DC8","DE6","AD5","CE2","EB3","AE7"}
+	if gra,err := NewGraphFromString(data);err == nil {
+		 fmt.Println(gra.BFSDistLimit("C","C",30))
 	}
 }
--- main.go
+++ main.go
@@ -11,30 +11,71 @@ func main()  {
 	//Ask
 	input := flag.String("input","AB5,BC4,CD8,DC8,DE6,AD5,CE2,EB3,AE7","Please Input the Data")
 	data := strings.Split(*input,",")
-	if gra,err := graph.NewGraphFromString(data);err == nil {
-		fmt.Println(gra.Len())
+	//Data Format
+	var gra *graph.Graph
+	var err error
+	var dist int
+	var route []string
+	if gra,err = graph.NewGraphFromString(data);err != nil {
+		fmt.Println(err)
+	}
+	// 1.	The distance of the route A-B-C.
+	route = strings.Split("A-B-C","-")
+	if dist,err =gra.CalcRouteDistance(route);err != nil {
+		fmt.Println("Output #1: ",err)
+	} else {
+		fmt.Println("Output #1: ",dist)
 	}
 
-	//Compute
 
-	//Answer
-}
+	// 2.	The distance of the route A-D.
+	route = strings.Split("A-D","-")
+	if dist,err =gra.CalcRouteDistance(route);err != nil {
+		fmt.Println("Output #2: ",err)
+	}else {
+		fmt.Println("Output #2: ",dist)
+	}
 
-func DataFormat()  {
+	// 3.	The distance of the route A-D-C.
+	route = strings.Split("A-D-C","-")
+	if dist,err =gra.CalcRouteDistance(route);err != nil {
+		fmt.Println("Output #3: ",err)
+	}else {
+		fmt.Println("Output #3: ",dist)
+	}
 
-	//Data Format"
-}
+	// 4.	The distance of the route A-E-B-C-D.
+	route = strings.Split("A-E-B-C-D","-")
+	if dist,err =gra.CalcRouteDistance(route);err != nil {
+		fmt.Println("Output #4: ",err)
+	}else {
+		fmt.Println("Output #4: ",dist)
+	}
 
-func Compute()  {
-	// CalcRouteDistance
+	// 5.	The distance of the route A-E-D.
+	route = strings.Split("A-E-D","-")
+	if dist,err =gra.CalcRouteDistance(route);err != nil {
+		fmt.Println("Output #5: ",err)
+	}else {
+		fmt.Println("Output #5: ",dist)
+	}
 
-	// Step Limit
+	// 6.	The number of trips starting at C and ending at C with a maximum of 3 stops.  In the sample data below, there are two such trips: C-D-C (2 stops). and C-E-B-C (3 stops).
+	fmt.Println("Output #6: ",gra.BFSTraverse("C","C",3,false))
 
-	// Shortest Route
+	// 7.	The number of trips starting at A and ending at C with exactly 4 stops.  In the sample data below, there are three such trips: A to C (via B,C,D); A to C (via D,C,D); and A to C (via D,E,B).
+	fmt.Println("Output #7: ",gra.BFSTraverse("A","C",4,true))
 
-	// Distance Limit
-}
+	// 8.	The length of the shortest route (in terms of distance to travel) from A to C.
+	fmt.Println("Output #8: ",gra.DifShortestPath("A","C"))
 
-func Answer()  {
-	// Println
+	// 9.	The length of the shortest route (in terms of distance to travel) from B to B.
+	if gra1,err := graph.NewGraphFromString(data);err == nil {
+		fmt.Println("Output #9: ", gra1.SameShortestPath("B"))
+	}
+
+	// 10.	The number of different routes from C to C with a distance of less than 30.  In the sample data, the trips are: CDC, CEBC, CEBCDC, CDCEBC, CDEBC, CEBCEBC, CEBCEBCEBC.
+	if gra2,err := graph.NewGraphFromString(data);err == nil {
+		fmt.Println("Output #10: ", gra2.BFSDistLimit("C", "C", 30))
+	}
 }
